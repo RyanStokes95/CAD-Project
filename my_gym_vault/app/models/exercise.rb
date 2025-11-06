d5c5e311d49c67fd4f550c598278110fb2e12a53
Ryan Stokes <ryanstokes560@gmail.com>
2025-11-06 18:31:45 +0000

    Added bootstrap styling and validation to exercises

--- my_gym_vault/app/models/exercise.rb
+++ my_gym_vault/app/models/exercise.rb
@@ -1,3 +1,7 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  # Validations for exercise attributes
+  validates :name, presence: true
+  validates :weight, numericality: { greater_than_or_equal_to: 0 }
+  validates :sets, :reps, numericality: { only_integer: true, greater_than: 0 }
 end
